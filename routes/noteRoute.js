65e6461f3dace0f60f052c4e85748e1d4a594a26
HunterHarvell <hunterharvell95@gmail.com>
2022-12-14 14:06:34 -0600

    changing the routes and merging the helper functions into the route files

--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -1,17 +1,57 @@
+// bringing in dependencies
 const noteRouter = require('express').Router();
+const { nanoid } = require('nanoid');
+const fs = require('fs');
 const data = require('../data/db.json');
 const { addNote, deleteNote } = require('../lib/data.js');
 
+// helper functions
+const notes = () => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(jsonPath, 'utf8', (err, data) => {
+            if (err) {
+                reject(jsonData);
+            } else {
+                resolve(JSON.parse(data));
+            }
+        });
+    });
+}
+
+function addNote(data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(jsonPath, JSON.stringify(data), 'utf-8', (err) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(true);
+            }
+        });
+    })
+}
+
+// get route
 noteRouter.get('/notes', (req, res) => {
-    res.send(data);
+    notes()
+        .then(notes => res.json(notes))
+        .catch((eer) => res.status(500).json({
+            error: err
+        }));
 });
-noteRouter.get('/', (req, res) => {
-    res.send('API HERE!');
-});
+
+// post route
 noteRouter.post('/notes', (req, res) => {
-    addNote(req.body);
-    res.json();
+    req.body['id'] = nanoid();
+    notes()
+        .then(notes => [...notes, req.body])
+        .then(addedNote => addNote(addedNote))
+        .then(isSuccess => res.json({success: isSuccess}))
+        .catch((err) => res.status(500).json({
+            error: err
+        }));
 });
+
+// delete route
 noteRouter.delete('/notes/:id', (req, res) => {
     deleteNote(req.params);
     res.json();
